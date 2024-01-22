51552a1dcbe35ee9afd7f8a31943d82f9269e8ef
Murat Simsek <muratsimseekk@gmail.com>
2024-01-22 23:46:16 +0300

    add items and sort method works correctly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,100 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
+
+   private static ArrayList<String> groceryList = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println("0 - Cikis yap .");
+        System.out.println("1- Eleman ekle .");
+        System.out.println("2- Eleman cikart .");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Seçiminizi yapınız: ");
+        int choice = scanner.nextInt();
+
+        do {
+
+            switch (choice){
+                case 0:
+                    System.out.println("Uygulamadan cikis yapiliyor ...");
+                    scanner.close();
+                    break;
+                case 1:
+                    addItem();
+                    printSorted();
+                    break;
+                case 2:
+                    removeItems();
+                    break;
+            }
+
+
+        }while (choice != 0);
+
+
+    }
+    public static void addItem(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Eklenmesini istediginiz elemanlari giriniz . ");
+
+        String input = scanner.nextLine();
+        String[] items = input.split(",");
+
+        for (String item : items){
+            if (groceryList.contains(item)){
+                System.out.println(item + " listede bulunuyor .");
+            }
+            else {
+                groceryList.add(item);
+                System.out.println(item + " basariyla eklendi . ");
+            }
+
+        }
+        Collections.sort(groceryList);
+
+    }
+    public static void removeItems (){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cikarilmasini istediginiz elemanlari giriniz . ");
+
+        String input = scanner.nextLine();
+        String[] items = input.split(",");
+
+        for (String item : items){
+            if (groceryList.contains(item)){
+                groceryList.remove(item);
+                System.out.println(item + " basariyla cikartildi . ");
+            }
+            else {
+                System.out.println(item + " listede bulunmuyor .");
+            }
+
+        }
+
+    }
+    public static void checkItemIsInList(){
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        if (groceryList.contains(input)){
+            System.out.println( input + " grocery listesinde bulunuyor . ");
+        }
+        else {
+            System.out.println(input + " grocery listesinde bulunmuyor . ");
+        }
+
+    }
+    public static void printSorted(){
+        System.out.println("Grocery Listesinin Sirlanmis Hali : ");
+        Collections.sort(groceryList);
+        for (String item : groceryList){
+            System.out.println(item);
+        }
+
     }
 }
